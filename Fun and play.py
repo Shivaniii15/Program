f04193822f0d2e9aadf9ffe2faf7fd12c6a76690
Shivaniii15 <st22421@ormiston.school.nz>
2023-06-22 14:51:55 +1200

    Update Fun and play.py

--- Fun and play.py
+++ Fun and play.py
@@ -3,3 +3,16 @@ from tkinter import *
 from tkinter import ttk
 
 root=Tk() #our window
+root.geometry("635x500")#size of window
+
+def quit(): #quit window
+    root.destroy()
+
+#running the code
+def main():
+    global root
+    root.config
+    root.title("Shivani <3") #title of the window
+    root.mainloop()#make sure it keeps looping
+
+main()
