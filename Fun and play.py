746ca9fb28c2b1bdd793628d164082cb4ff63034
Shivaniii15 <st22421@ormiston.school.nz>
2023-06-29 21:07:21 +1200

    Update Fun and play.py

--- Fun and play.py
+++ Fun and play.py
@@ -16,8 +16,23 @@ canvas1.pack(fill="both", expand=True)
 canvas1.create_image(0, 0,image=bg, anchor="nw")
 
 #adding buttons
-button1= Button (100, 40, root, text="Quit", command=quit)
+#the basic ones
+button1= Button (root, text="Quit", command=quit)
+button1.place(x=550, y=376)
+button2= Button (root, text="Home")
+button2.place(x=95, y=376)
 
+#the activity ones
+button3= Button (root, text="TOPICS!")
+button3.place(x=170, y=174)
+button4= Button (root, text="FUN FACTS")
+button4.place(x=270, y=174)
+button5= Button (root, text="QUIZES!")
+button5.place(x=400, y=100)
+button6= Button (root, text="REPORT!")
+button6.place
+button7= Button (root, text="NOTES AND SUMMARY")
+button3.place
 
 def quit(): #quit window
     root.destroy()
