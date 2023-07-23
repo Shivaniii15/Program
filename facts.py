d86a22116eb606389517519e684f0a9f24555d99
Shivaniii15 <st22421@ormiston.school.nz>
2023-07-24 11:38:49 +1200

    ..

--- facts.py
+++ facts.py
@@ -1,6 +1,7 @@
 #import everything and ttk to get started with the code
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
 import tkinter.font as font
 
 root=Tk() #our window
@@ -38,6 +39,20 @@ button1.place(x=600, y=423)
 button2= Button (root, text="Home", command=home, bg='#F14975', fg='white', activebackground='#288C70', activeforeground='white')
 button2.place(x=185, y=423)
 
+#adding a text box with a scrollbar
+text= tk.Text(root, height=6, width=12 )
+scroll= tk.Scrollbar(root)
+text.configure(yscrollcommand=scroll.set)
+text.pack(side=tk.LEFT)
+
+scroll.config(command=text.yview)
+scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+insert_text= """1000 is the only number:
+from 0 to 1000 that has 
+an “a” in it!"""
+text.insert(tk.END,insert_text)
+
 #applying font
 button1['font']= myFont
 button2['font']= myFont
@@ -47,6 +62,6 @@ def main():
     global root
     root.config
     root.title("Shivani <3") #title of the window
-    root.mainloop()#make sure it keeps looping
-
+    tk.mainloop()#ensures that the program keeps running
 main()
+
