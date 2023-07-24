c518d73656af4088018593bb9095dd832decd3a3
Shivaniii15 <st22421@ormiston.school.nz>
2023-07-25 10:16:57 +1200

    .

--- facts.py
+++ facts.py
@@ -1,7 +1,7 @@
 #import everything and ttk to get started with the code
 from tkinter import *
-from tkinter import ttk
 import tkinter as tk
+from tkinter import ttk
 import tkinter.font as font
 
 root=Tk() #our window
@@ -39,19 +39,18 @@ button1.place(x=600, y=423)
 button2= Button (root, text="Home", command=home, bg='#F14975', fg='white', activebackground='#288C70', activeforeground='white')
 button2.place(x=185, y=423)
 
-#adding a text box with a scrollbar
-text= tk.Text(root, height=6, width=12 )
-scroll= tk.Scrollbar(root)
-text.configure(yscrollcommand=scroll.set)
-text.pack(side=tk.LEFT)
+#adding labels
+label1=Label(root, text="1000 is the only number from 0 to 1000 that has an “a” in it!", wraplength=100, width= 13,
+      bg="#CB6CE6", font=("helvetica", 10)).place(x=145, y=145)
+label2=Label(root, text="There is no Roman numeral for zero!", wraplength=100, width= 13,
+      bg="#FFCE4F", font=("helvetica", 10)).place(x=368, y=145)
+label3=Label(root, text="“Forty” is the only number that is spelt with letters arranged in alphabetical order!", wraplength=100, width= 12,
+      bg="#7B99CE", font=("helvetica", 9)).place(x=576, y=130)
+label4=Label(root, text="“Every odd number has an “e” in it!", wraplength=100, width= 12,
+      bg="#E5645E", font=("helvetica", 10)).place(x=149, y=312)
+label5=Label(root, text="The symbol for division (i.e.÷) is called an obelus!", wraplength=100, width= 12,
+      bg="#288C70", font=("helvetica", 10)).place(x=370, y=312)
 
-scroll.config(command=text.yview)
-scroll.pack(side=tk.RIGHT, fill=tk.Y)
-
-insert_text= """1000 is the only number:
-from 0 to 1000 that has 
-an “a” in it!"""
-text.insert(tk.END,insert_text)
 
 #applying font
 button1['font']= myFont
